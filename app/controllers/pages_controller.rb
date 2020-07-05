46fe1d2d1b4244937cfd8d909020117af2bfb0dc
Anni Chen <anni.chen@duke.edu>
2020-07-05 14:31:47 -0400

    Installed devise gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     def show 
+        puts "LOOK AT HERE"
+        puts params[:pages]
         render template: "pages/#{params[:page]}"
     end
 end
