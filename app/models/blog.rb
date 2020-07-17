ed7728b74347232c4a6deb76999a979effd98d81
Anni Chen <anni.chen@duke.edu>
2020-07-17 00:02:29 -0400

    Resolve situation blogs are deleted, prev and next

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,10 @@
 class Blog < ApplicationRecord
     has_rich_text :body
+    def next_post
+        self.class.where("created_at < ?", created_at).order(created_at: :desc).first
+    end
+      
+    def previous_post
+        self.class.where("created_at > ?", created_at).order(created_at: :asc).first
+    end
 end
